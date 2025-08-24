37cd4bc9d408205d7404cf4f9a53648912e44ea2
Lautaro Mendieta <lemendieta96@gmail.com>
2025-08-24 09:57:47 -0300

    Use buildx in local build script

--- scripts/linux_build.sh
+++ scripts/linux_build.sh
@@ -3,4 +3,7 @@
 readonly PROJECT_DIRECTORY="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && cd .. && pwd)"
 readonly IMAGE_NAME='zephyr-docker-image:local'
 
-docker build -f "${PROJECT_DIRECTORY}/Dockerfile" -t "${IMAGE_NAME}" "${PROJECT_DIRECTORY}"
+docker buildx build --platform linux/amd64 --no-cache \
+                    -f "${PROJECT_DIRECTORY}/Dockerfile" \
+                    -t "${IMAGE_NAME}" \
+                    "${PROJECT_DIRECTORY}"
